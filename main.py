5e0713ff4acaf0754141d158d5a682c01ea67b4e
Bhavya kalra <kalrabhavya13@gmail.com>
2020-11-19 01:47:50 +0530

    new commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
+
+
+
+# engine = pyttsx3.init()
+# engine.setProperty("rate", 178)
+# engine.say("I am the text spoken after changing the speech rate.")
+# engine.runAndWait()
 
 
 engine = pyttsx3.init('sapi5')
@@ -23,5 +31,27 @@ def wishme():
 
     speak("I am Jarvis Sir. Please tell me how I can help you")
 
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+
+        # r.pause_threshold = 1
+        
+        r.adjust_for_ambient_noise(source, duration = 1)
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n") 
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...") 
+        return "None"
+    return query
+
+
 if __name__=="__main__" :
-    speak("Bhvaya is good boy")
+    speak("Hello sir")
+    wishme()
+    takecommand()
